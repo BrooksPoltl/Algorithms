5d84e3f0a0f1b12cd66b0e621483631ecd32b99c
brooks <bpoltl1@gmail.com>
2019-04-01 16:10:42 -0500

    completed rock paper scissors

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -3,8 +3,24 @@
 import sys
 
 def rock_paper_scissors(n):
-  pass 
-
+  if n == 0:
+    return [[]]
+  count = 1
+  answer = [['rock'], ['paper'], ['scissors']]
+  if n == 1:
+    return answer
+  possibilities = [['rock'],['paper'],['scissors']]
+  def recursion_helper(list):
+    new_list = []
+    for item in answer:
+      for possibility in possibilities:
+        new_list.append(item + possibility)
+    return new_list
+  while count < n:
+    new_list = recursion_helper(answer)
+    answer = new_list
+    count += 1
+  return answer
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
